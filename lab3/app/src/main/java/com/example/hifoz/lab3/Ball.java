8fc00d7e540e3e58ce5507eb593f1ba676089ad6
Michael Bråten <braaten.michael@outlook.com>
2018-02-22 12:14:32 +0100

    . Added collision checking. Also added temporary movement to the ball, until I start working on sensor integration.

--- lab3/app/src/main/java/com/example/hifoz/lab3/Ball.java
+++ lab3/app/src/main/java/com/example/hifoz/lab3/Ball.java
@@ -2,14 +2,16 @@ package com.example.hifoz.lab3;
 
 import android.graphics.Paint;
 
-
+/**
+ * Data structure containg data on a ball
+ */
 public class Ball{
-    public float x;
-    public float y;
-    public float radius;
+    public int x;
+    public int y;
+    public int radius;
     public Paint paint;
 
-    public Ball(float x, float y, float radius, Paint paint){
+    public Ball(int x, int y, int radius, Paint paint){
         this.x = x;
         this.y = y;
         this.radius = radius;
--- lab3/app/src/main/java/com/example/hifoz/lab3/BouncingBallView.java
+++ lab3/app/src/main/java/com/example/hifoz/lab3/BouncingBallView.java
@@ -4,35 +4,101 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
-import android.os.AsyncTask;
+import android.graphics.Point;
+import android.graphics.PointF;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.view.View;
 
 public class BouncingBallView extends View {
+
     int vWidth;
     int vHeight;
 
     int bgPadding;
-    Rect bgRect;
     Paint bgPaint;
 
-    Ball ball;
-
+    public static Ball ball;
 
     public BouncingBallView(Context context) {
         super(context);
 
         bgPadding = 10;
-        bgRect = new Rect();
         bgPaint = new Paint();
         bgPaint.setColor(Color.BLACK);
 
         Paint ballPaint = new Paint();
         ballPaint.setColor(Color.RED);
-        ball = new Ball(0.5f, 0.5f, 0.05f, ballPaint);
+        ball = new Ball(50, 50, 5, ballPaint);
+
+        startAnimation();
 
 
     }
+
+    long mAnimStartTime;
+
+    Handler mHandler = new Handler();
+    Runnable mTick = new Runnable() {
+        public void run() {
+            invalidate();
+            mHandler.postDelayed(this, 20); // 20ms == 60fps
+        }
+    };
+
+
+    void startAnimation() {
+        mAnimStartTime = SystemClock.uptimeMillis();
+        mHandler.removeCallbacks(mTick);
+        mHandler.post(mTick);
+    }
+
+    void stopAnimation() {
+        mHandler.removeCallbacks(mTick);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        updateBall();
+
+        canvas.drawRect(bgPadding, bgPadding, vWidth - bgPadding, vHeight - bgPadding, bgPaint);
+        canvas.drawCircle(ball.x, ball.y, ball.radius, ball.paint);
+    }
+
+    int dirX = -1; // todo: temp, only to be used until we get sensors working
+    int dirY = -1; // todo: temp, only to be used until we get sensors working
+    private void updateBall() {
+        Point velocity = new Point(dirX, dirY);
+
+        Point newPos = new Point(ball.x + velocity.x, ball.y + velocity.y);
+
+        int collisionResult = testCollision(newPos);
+        if(collisionResult == 1) {
+            dirX = -dirX;
+            handleCollision();
+        } else if (collisionResult == 2) {
+            dirY = -dirY;
+            handleCollision();
+        } else {
+            ball.x = newPos.x;
+            ball.y = newPos.y;
+        }
+
+    }
+
+    private int testCollision(Point pos){
+        if (pos.x <= bgPadding + ball.radius || pos.x >= vWidth - bgPadding - ball.radius)
+            return 1;
+        else if(pos.y <= bgPadding + ball.radius || pos.y >= vHeight - bgPadding - ball.radius)
+            return 2;
+        return 0;
+    }
+
+
+    private void handleCollision(){
+
+    }
+
 
     @Override
     protected void onSizeChanged(int w, int h, int oldW, int oldH){
@@ -41,30 +107,11 @@ public class BouncingBallView extends View {
         vWidth = w;
         vHeight = h;
 
-        bgRect.set(bgPadding, bgPadding, vWidth - bgPadding, vHeight - bgPadding);
-    }
+        ball.x = vWidth / 2;
+        ball.y = vHeight / 2;
+        ball.radius = vWidth / 20;
 
-    @Override
-    protected void onDraw(Canvas canvas){
-        canvas.drawRect(bgRect, bgPaint);
-        canvas.drawCircle(ball.x * vWidth, ball.y * vHeight , ball.radius * vWidth, ball.paint);
     }
 
 
-
-
-    protected void onMeasure(final int widthMeasureSpec, final int heightMeasureSpec) {
-        vHeight = MeasureSpec.getSize(heightMeasureSpec);
-        vWidth = MeasureSpec.getSize(widthMeasureSpec);
-
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-    }
-
-    private static class BouncingBallTask extends AsyncTask<Void, Integer, Integer> {
-
-        @Override
-        protected Integer doInBackground(Void... voids) {
-            return null;
-        }
-    }
 }
